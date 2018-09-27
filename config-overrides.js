6ca545e5075c9d2958e50f58f35deefb8ecbecd8
Aleksey Gurtovoy <agurtovoy@acm.org>
2018-09-27 17:49:33 -0500

    Dev: plug in Airbnb eslint config (with some tweaks)

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,6 @@
+const rewireEslint = require('react-app-rewire-eslint'); // eslint-disable-line import/no-extraneous-dependencies
+
 module.exports = (config, env) => {
+  config = rewireEslint(config, env);
   return config;
-}
+};
