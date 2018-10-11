0adfe304181dbe58dcab0cacb6f589d8f27fa8ff
hmahmoud3 <hassan.mahmoud3@gmail.com>
2018-10-11 14:53:10 -0400

    fixed styling

--- src/components/RoundButton.js
+++ src/components/RoundButton.js
@@ -4,17 +4,12 @@ import styled, { css } from 'styled-components';
 const RoundButton = styled.div`
   height: 50px;
   width: 50px;
-  padding: 0px 30px;
-  box-shadow: 0 2px 4px 0 rgba(125, 125, 125, 0.2);
-  border: solid 1px #ededed;
-  font-size: 12px;
-  font-weight: 800;
-  display: inline-flex;
-  align-items: center;
+  background-color: ${props => props.theme.colors.primary};
+  border-radius: 50%;
+  display: flex;
   justify-content: center;
-  border-radius: 4px;
-  color: #7d7d7d;
-  text-transform: uppercase;
+  align-items: center;
+  box-shadow: 0 2px 2px 0 rgba(125, 125, 125, 0.2);
   cursor: pointer;
 
   ${props =>
@@ -26,6 +21,4 @@ const RoundButton = styled.div`
     `};
 `;
 
-export default ({ label, ...restProps }) => (
-  <RoundButton {...restProps}>{label}</RoundButton>
-);
+export default ({ label, ...restProps }) => <RoundButton {...restProps} />;
