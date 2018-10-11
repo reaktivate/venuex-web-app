a6d4270ab160ff133c286c878e69da262e0d7c25
hmahmoud3 <hassan.mahmoud3@gmail.com>
2018-10-11 14:53:35 -0400

    added styling

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -2,13 +2,16 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import Button from './Button';
 import RoundButton from './RoundButton';
+import plusIcon from '../assets/plus.svg';
+import filterIcon from '../assets/filter.svg';
 
 const TableHeader = styled.div`
   height: 40px; /* not sure what to set this as */
   background-color: #fcfbfc;
   padding: 20px 15px;
   display: flex;
-  justify-content: space-between
+  justify-content: space-between;
+  align-items: center
     ${props =>
       props.kind === 'primary' &&
       css`
@@ -16,14 +19,49 @@ const TableHeader = styled.div`
       `};
 `;
 
-const ActionButtonGroup = styled.div``;
+const FilterLabel = styled.div` 
+font-family: Montserrat;
+font-size: 12px;
+font-weight: bold;
+font-style: normal;
+font-stretch: normal;
+line-height: normal;
+letter-spacing: normal;
+text-align: center;
+color: #7d7d7d;
+display: flex;
+align-items: center;
+justify-content: space-around
+}`;
+
+const AddIcon = styled.img`
+  height: 17px;
+  width: 17px;
+`;
+
+const ActionButtonGroup = styled.div`
+  display: flex;
+  width: 111px;
+  justify-content: space-between;
+`;
 
 export default ({ label, ...restProps }) => (
   <TableHeader {...restProps}>
     {' '}
-    <Button label="Filter" />
+    <Button
+      label={
+        <FilterLabel>
+          <AddIcon src={filterIcon} />
+          Filter
+        </FilterLabel>
+      }
+      kind="white"
+    />
     <ActionButtonGroup>
-      <RoundButton label="+" /> <RoundButton label="U" />
+      <RoundButton>
+        <AddIcon src={plusIcon} />
+      </RoundButton>
+      <RoundButton label="U" />
     </ActionButtonGroup>
   </TableHeader>
 );
