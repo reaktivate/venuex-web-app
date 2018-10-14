12e6c08f46e6dd884762059bf0f0f691d707e2be
hmahmoud3 <hassan.mahmoud3@gmail.com>
2018-10-14 13:37:09 -0400

    fixed TableCol and converted to TableCell

--- src/routes/ManageStaff.js
+++ src/routes/ManageStaff.js
@@ -5,7 +5,7 @@ import ManageStaffHeader from 'components/ManageStaffHeader.js';
 import TableRow from 'components/Table/TableRow.js';
 import ColTitle from 'components/Table/ColTitle.js';
 import CheckBox from 'components/Checkbox.js';
-import TableCol from 'components/Table/TableCol.js';
+import TableCell from 'components/Table/TableCell.js';
 import PermissionsIcons from 'components/PermissionsIcons.js';
 
 class ManageStaff extends PureComponent {
@@ -17,27 +17,27 @@ class ManageStaff extends PureComponent {
         <Table>
           <ManageStaffHeader />
           <TableRow>
-            <TableCol width="5%">
+            <TableCell width="5%">
               <CheckBox />
-            </TableCol>
-            <TableCol width="20%">
+            </TableCell>
+            <TableCell width="20%">
               <ColTitle title="Name" selected sortable color="#c0b59d" />
-            </TableCol>
-            <TableCol width="20%">
+            </TableCell>
+            <TableCell width="20%">
               <ColTitle title="Email" sortable color="#c0b59d" />
-            </TableCol>
+            </TableCell>
 
-            <TableCol width="20%" content={<ColTitle title="Permissions" />} />
-            <TableCol
+            <TableCell width="20%" content={<ColTitle title="Permissions" />} />
+            <TableCell
               width="20%"
               content={<ColTitle title="Date Added" sortable color="#c0b59d" />}
             />
           </TableRow>
           <TableRow>
-            <TableCol width="5%" content={<CheckBox />} />
-            <TableCol content="First Column" width="20%" emphasize />
-            <TableCol content="Second Column" width="20%" emphasize />
-            <TableCol
+            <TableCell width="5%" content={<CheckBox />} />
+            <TableCell content="First Column" width="20%" emphasize />
+            <TableCell content="Second Column" width="20%" emphasize />
+            <TableCell
               content={
                 <PermissionsIcons
                   color="#c0b59d"
@@ -49,14 +49,14 @@ class ManageStaff extends PureComponent {
               }
               width="20%"
             />
-            <TableCol content="Fourth Column" width="20%" />
+            <TableCell content="Fourth Column" width="20%" />
           </TableRow>
           <TableRow>
-            <TableCol width="5%" content={<CheckBox />} />
-            <TableCol content="First Column" width="20%" />
-            <TableCol content="Second Column" width="20%" />
-            <TableCol content="Third Column" width="20%" />
-            <TableCol content="Fourth Column" width="20%" />
+            <TableCell width="5%" content={<CheckBox />} />
+            <TableCell content="First Column" width="20%" />
+            <TableCell content="Second Column" width="20%" />
+            <TableCell content="Third Column" width="20%" />
+            <TableCell content="Fourth Column" width="20%" />
           </TableRow>
         </Table>
       </SidebarLayout>
