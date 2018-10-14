5d54552654c6808d93d7aa1c61fdbbe7204d0881
hmahmoud3 <hassan.mahmoud3@gmail.com>
2018-10-14 14:20:17 -0400

    table api

---
+++ src/components/Table/TableCell.js
@@ -0,0 +1,32 @@
+import styled, { css } from 'styled-components';
+
+const TableCell = styled.div`
+  font-family: Montserrat;
+  font-size: 12px;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: 0.3px;
+  color: #888888;
+  display: flex;
+  min-width: 50px;
+  ${props =>
+    props.selected === true &&
+    css`
+      font-weight: bold;
+      color: #181818;
+    `};
+  ${props =>
+    props.width &&
+    css`
+      width: ${props.width};
+    `};
+  ${props =>
+    props.emphasize === true &&
+    css`
+      font-weight: bold;
+      color: #222222;
+    `};
+`;
+
+export default TableCell;
