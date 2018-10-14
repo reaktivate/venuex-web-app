c01d3285f9304ce6bf008739894eaf71d681bc23
hmahmoud3 <hassan.mahmoud3@gmail.com>
2018-10-14 14:20:17 -0400

    table api

---
+++ src/components/Table/index.js
@@ -0,0 +1,10 @@
+import TableCell from './TableCell';
+import TableRow from './TableRow';
+import ColTitle from './ColTitle';
+import Table from './Table';
+
+Table.Row = TableRow;
+Table.HeaderCell = ColTitle;
+Table.Cell = TableCell;
+
+export default Table;
